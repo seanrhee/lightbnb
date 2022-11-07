564e4b6212ce3116746989d8dd9194250d5666e7
Sean Lee <lee.sean.cw@gmail.com>
2022-11-07 23:55:07 +0000

    uncomment necessary code in 01_seeds.sql

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,19 +1,19 @@
--- insert into users (name, email, password)
--- values ('Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
--- ('Louisa Meyer', 'jacksonrose@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
--- ('Dominic Parks', 'victoriablackwell@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+insert into users (name, email, password)
+values ('Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Louisa Meyer', 'jacksonrose@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Dominic Parks', 'victoriablackwell@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
--- insert into properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
--- values (1, 'Speed lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', '930', 6, 4, 8, 'Canada', '536 Namsub Highway', 'Sotboske', 'Quebec', '28142', true),
--- (1, 'Blank corner', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', '85234', 6, 6, 7, 'Canada', '651 Nami Road', 'Bohbatev', 'Alberta', '83680', true),
--- (2, 'Default Property', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', '60', 1, 2, 3, 'Canada', '123 Fake St', 'Faketon', 'Fakeville', '12345', false);
+insert into properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+values (1, 'Speed lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', '930', 6, 4, 8, 'Canada', '536 Namsub Highway', 'Sotboske', 'Quebec', '28142', true),
+(1, 'Blank corner', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', '85234', 6, 6, 7, 'Canada', '651 Nami Road', 'Bohbatev', 'Alberta', '83680', true),
+(2, 'Default Property', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', '60', 1, 2, 3, 'Canada', '123 Fake St', 'Faketon', 'Fakeville', '12345', false);
 
--- insert into reservations (start_date, end_date, property_id, guest_id)
--- values ('2000-01-11', '2000-01-12', 2, 1),
--- ('2001-01-11', '2001-01-12', 2, 2),
--- ('2011-11-11', '2011-11-12', 1, 3);
+insert into reservations (start_date, end_date, property_id, guest_id)
+values ('2000-01-11', '2000-01-12', 2, 1),
+('2001-01-11', '2001-01-12', 2, 2),
+('2011-11-11', '2011-11-12', 1, 3);
 
 insert into property_reviews (guest_id, property_id, reservation_id, rating, message)
-values (3, 2, 4, 3, 'message'),
-(2, 2, 5, 4, 'message'),
-(3, 1, 6, 4, 'message');
+values (3, 2, 1, 3, 'message'),
+(2, 2, 1, 3, 'message'),
+(3, 1, 2, 3, 'message');
